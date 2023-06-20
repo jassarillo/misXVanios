1a3a08cb22fbdf8ba0242795b089f5c3af78e295
Julio Cesar <jassito33c@g.com>
2023-06-20 15:31:41 -0600

    conexion

--- buscaFolio/config.php
+++ buscaFolio/config.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "root", "xv_anios");
+//$conn = new mysqli("localhost", "root", "root", "xv_anios");
+$conn = new mysqli("143.198.231.211", "xv", "xv", "UYUhYCwHo4OPHnILNDxP");
 
 if ($conn->connect_error) {
     die('Error de conexion ' . $conn->connect_error);
