06f6066375b5376faf560880a3322aa3e4598855
Julio Cesar <jassito33c@g.com>
2023-06-20 15:50:01 -0600

    conexion

--- buscaFolio/config.php
+++ buscaFolio/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //$conn = new mysqli("localhost", "root", "root", "xv_anios");
-$conn = new mysqli("143.198.231.211", "xv", "xv", "UYUhYCwHo4OPHnILNDxP");
+$conn = new mysqli("143.198.231.211", "xv", "UYUhYCwHo4OPHnILNDxP", "xv");
 
 if ($conn->connect_error) {
     die('Error de conexion ' . $conn->connect_error);
